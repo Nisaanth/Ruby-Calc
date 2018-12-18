7117eb44a739844fcb302d993f909ca271e44290
Tech-A03x <tech-a03x@tech-a03s-air.lan>
2018-12-18 22:54:32 +0000

    Added comments

--- calc.rb
+++ calc.rb
@@ -1,10 +1,14 @@
+# inputs first number 
 puts "enter first number: "
 num1 = gets.chomp().to_f
+# inputs operator 
 puts "enter operator or if you would like to square root, type: 'sr' or if you would like to square, type: 'power': "
 op = gets.chomp()
+# inputs second number 
 puts "enter second number: "
 num2 = gets.chomp().to_f
 
+# if else statement for operators
 if op == "+"
   puts (num1 + num2)
 elsif op == "-"
@@ -20,6 +24,7 @@ elsif op == "power"
 else 
   puts "Invalid operator"
 end
+
 
 
 
